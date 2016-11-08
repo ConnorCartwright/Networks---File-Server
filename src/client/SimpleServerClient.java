1d5639bc729c8b51c33c8fff5a0e83cd55736e3d
Connor Cartwright <connorcart@gmail.com>
2016-11-08 19:34:11 +0000

    Added client

---
+++ src/client/SimpleServerClient.java
@@ -0,0 +1,39 @@
+package client;
+
+import java.awt.*;
+import java.io.File;
+import java.io.IOException;
+import java.net.URL;
+import java.nio.file.Paths;
+
+/**
+ * Created by connor on 07/11/2016.
+ */
+public class SimpleServerClient {
+
+    public static void main(String[] args) {
+        if (args.length > 0) {
+            SimpleServerClient ssc = new SimpleServerClient(args[0]);
+        } else {
+            System.out.println("No URL specified!");
+        }
+    }
+
+    public SimpleServerClient(String url) {
+        Desktop d = Desktop.getDesktop();
+        try { // try and get the live site
+            URL u = new URL(url);
+            d.browse(u.toURI());
+            System.out.println("GET " +  url + " HTTP/1.1 200 OK");
+        } catch (Exception e) {
+            try { // else try and find the file locally
+                File f = new File("resources/" + Paths.get(url));
+                d.edit(f);
+                System.out.println("GET " +  url + " HTTP/1.1 200 OK");
+                System.out.println("Resource found locally!");
+            } catch (IOException e1) { // else the file is neither live or local
+                System.out.println("404, file not found");
+            }
+        }
+    }
+}
